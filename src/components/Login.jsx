0ed680f150e82e4345701cab3f55bd5b444f1213
Adhar1777 <149721236+AdharBattulwar@users.noreply.github.com>
2024-08-19 13:26:40 +0530

    Ui Change In Login and Registration Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 // import axiosInstance from "../client";
 import { useCookies } from 'react-cookie';
+import "../Stylesheet/Login.css"
 
 const Login = () => {
   const navigate = useNavigate();
@@ -36,49 +37,49 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full h-auto flex justify-center align-center pt-16">
-      <div className="w-1/2 h-1/2 p-10 my-6 overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-        <h1 className="flex justify-center text-2xl font-bold">
+    <div className="LoginWrapper w-full h-full pr-20 flex justify-end align-center pt-16">
+      <div className="relative w-1/3 h-5/6 py-10 px-6 my-6 overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+        <h1 className="relative h-auto w-full flex items-center justify-center text-2xl font-bold">
           Login To Your Account
         </h1>
-        <div className="flex flex-col h-full mt-4 mb-2 justify-center align-center">
+        <div className="relative flex flex-col h-full justify-center align-center">
           <form
             onSubmit={handleSubmit}
             method="post"
-            className="flex flex-col w-full h-full justify-center align-center gap-4 pt-4"
+            className="relative h-full w-auto flex flex-col justify-center align-center gap-4 pt-4"
           >
             <input
               type="email"
-              className="rounded-md outline-none px-4 py-2 border-2 border-gray-200 hover:bg-gray-200"
+              className="h-10 rounded-md outline-none px-4 py-2 border-2 border-gray-200 hover:bg-gray-200"
               name="Email"
               required
               placeholder="Email Address"
             />
             <input
               type="password"
-              className="rounded-md outline-none px-4 py-2 border-2 border-gray-200 hover:bg-gray-200"
+              className="h-10 rounded-md outline-none px-4 py-2 border-2 border-gray-200 hover:bg-gray-200"
               name="Password"
               required
               placeholder="Password"
             />
-            <div className="flex justify-center">
+            <div className="relative h-auto flex justify-center">
               <input
                 type="submit"
                 value="Login"
-                className="px-4 py-2 bg-blue-500 text-white rounded-xl w-1/2 mt-4 hover:border-blue-200"
+                className="h-10 px-4 py-2 bg-blue-500 text-white rounded-xl w-1/2 hover:border-blue-200"
               />
             </div>
-            <div className="flex justify-between mt-1">
-              <div>
+            <div className="relative h-auto mt-8 flex justify-between">
+              <div className="w-1/2">
                 Don't Have An Account?
                 <Link to="/registration" className="text-blue-800 font-bold">
                   &nbsp;Sign Up
                 </Link>
               </div>
-              <div>
+              <div className="w-1/2">
                 <a
                   href="/forgotPassword"
-                  className="text-gray-400 font-bold hover:text-gray-600"
+                  className="flex justify-center items-center text-gray-400 font-bold hover:text-gray-600"
                 >
                   &nbsp;Forgot Password!
                 </a>
--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "../Stylesheet/Registration.css"
 
 function Registration() {
     const navigate = useNavigate();
@@ -32,39 +33,39 @@ function Registration() {
     }
 
   return (
-    <div className="w-full h-auto flex justify-center align-center">
-      <div className="w-1/2 h-1/2 p-10 my-6 rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-        <h1 className="flex justify-center text-2xl font-bold">
+    <div className="RegistrationWrapper w-full h-full pr-20 pt-16 flex justify-end align-center">
+      <div className="absolute w-1/3 h-5/6 py-10 px-6 rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+        <h1 className="relative h-auto w-full flex justify-center text-2xl font-bold">
           Register For Your Account
         </h1>
-        <div className="flex flex-col h-auto mt-5 justify-center align-center">
+        <div className="relative h-full w-full flex flex-col mt-5 justify-center align-center">
           <form
             onSubmit={getRegistered}
             method="post"
-            className="flex flex-col w-full h-full justify-center align-center gap-4 pt-4"
+            className="relative flex flex-col w-full h-full justify-center align-center gap-4"
           >
             <input
               type="email"
-              className="rounded-md outline-none px-4 py-2 border-2 border-gray-200 hover:border-blue-200"
+              className="relative h-10 rounded-md outline-none px-4 py-2 border-2 border-gray-200 hover:border-blue-200"
               name="Email"
               placeholder="Email Address"
             />
             <input
               type="password"
-              className="rounded-md outline-none px-4 py-2 border-2 border-gray-200 hover:border-blue-200"
+              className="relative h-10 rounded-md outline-none px-4 py-2 border-2 border-gray-200 hover:border-blue-200"
               name="Password"
               placeholder="Password"
             />
-            <div className="flex justify-center">
+            <div className="realtive h-10 flex justify-center">
               <input
                 type="submit"
                 value="Sign Up"
-                className="px-4 py-2 bg-gray-100 rounded-xl w-1/2 mt-4 hover:bg-gray-200 hover:border-blue-200"
+                className="relative h-10 px-4 py-2 bg-gray-100 rounded-xl w-1/2 hover:bg-gray-200 hover:border-blue-200"
               />
             </div>
-            <div className="flex mt-1">
+            <div className="realtive h-auto w-full flex items-center justify-center mt-20">
               Already Have An Account?
-              <a href="/login" className="text-blue-800 font-bold">
+              <a href="/login" className="w-1/2 flex items-center justify-center text-blue-800 font-bold">
                 &nbsp;Sign In
               </a>
             </div>
